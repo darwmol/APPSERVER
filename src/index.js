b676d15834e9e1a57564b15332c4b88c35768c65
Darwin <darwmolina@gmail.com>
2024-09-11 17:01:30 -0500

    APPSERVER for MarValen

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const userRoutes = require ("./routes/user");
+
+
+const port = process.env.PORT || 9000;
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Welcome to server");
+});
+
+//middleware
+app.use(express.json());
+app.use('/api', userRoutes)
+
+
+
+//mongobd connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to BD"))
+  .catch((error) => console.error(error));
+
+app.listen(port, () => console.log("server listening on port", port));
